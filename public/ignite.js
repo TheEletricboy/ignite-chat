6d273eab304103b53885027e9e3bc1cc093bf5dd
Miguel Vieira <miguelvieira1997@hotmail.com>
2020-05-17 15:07:45 +0100

    Igntie.js - moved logic inside js file

--- public/ignite.js
+++ public/ignite.js
@@ -1,32 +1,79 @@
-// var myName = prompt("Enter your Name: ", "Ex: John Doe");
 
-//      function sendMessage() {
-//           // get message
-//           var message = document.getElementById("message").value;
-
-//           //save in database
-//           firebase.database().ref('messages').push().set({
-//                "sender": myName,
-//                "message": message
-//           });
-
-//           // prevent form from submitting
-//           return false;
-//      }
-
-//      //listen for incoming messages
-//      firebase.database().ref("messages").on("child_added", function (snapshot) {
-//           var htmlCustom = "";
-//           htmlCustom += "<li>";
-//                htmlCustom += snapshot.val().sender + ": " + snapshot.val().message;
-//           htmlCustom += "</li>";
-
-//           document.getElementById("messages").innerHTML += htmlCustom;
-//      });
+// ----------------- Public Vars -----------------
+var myName = prompt("Enter your Name:");
 
 
-$(document).ready(function(){
-    $('#action_menu_btn').click(function(){
-        $('.action_menu').toggle();
-    });
+
+
+
+if (!myName) {
+   myName = "Anonymous";
+}
+
+if (myName) {
+   $( document ).ready(function() {
+       console.log( "ready!" );
+       autoScrollDown();
+   });
+}
+
+
+// ----------------- Functions -----------------
+
+// send the message
+$('#message_form').submit(function(e){
+   // prevent form from submitting
+   e.preventDefault();
+   // get message
+   var messageInput = document.getElementById("message");
+   var message = messageInput.value;
+
+   console.log(message);
+
+   //save in database
+   firebase.database().ref('messages').push().set({
+       "sender": myName,
+       "message": message
+   });
+
+   // delete message after sending
+   messageInput.value = '';
 });
+
+//listen for incoming messages
+firebase.database().ref("messages").on("child_added", function (snapshot) {
+     var htmlCustom = "";
+     // give each message a unique ID
+     htmlCustom += "<li id='message-" + snapshot.key + "'>";
+          //show delete vutton if message is sent by me
+          if (snapshot.val().sender === myName) {
+               htmlCustom += "<button data-id='" + snapshot.key + "' onclick='deleteMessage(this)'>";
+                    htmlCustom += "Delete"
+               htmlCustom += "</button>";
+          }
+          htmlCustom += snapshot.val().sender + ": " + snapshot.val().message;
+     htmlCustom += "</li>";
+
+     document.getElementById("messages").innerHTML += htmlCustom;
+     autoScrollDown();
+});
+
+function deleteMessage(self) {
+     //get message ID
+     var messageId = self.getAttribute("data-id");
+
+     //delete message
+     firebase.database().ref("messages").child(messageId).remove();
+}
+
+// autoscroll down function
+function autoScrollDown() {
+    var cardBody = document.getElementsByClassName('msg_card_body')[0];
+    cardBody.scrollTop = cardBody.scrollHeight;
+}
+
+//attach listener for delete message
+firebase.database().ref("messages").on("child_removed", function (snapshot) {
+     // remove message node
+     document.getElementById("message-" + snapshot.key).innerHTML = "This message has been removed";
+})
