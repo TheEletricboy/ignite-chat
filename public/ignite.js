913009ebc8954a7738127f4e0dde24762d54c7a9
Miguel Vieira <miguelvieira1997@hotmail.com>
2020-05-24 23:47:16 +0100

    Moved messages logic to file - Js

--- public/ignite.js
+++ public/ignite.js
@@ -51,13 +51,6 @@ if (myName) {
 }
 
 
-function init() {
-    cycleBackground();
-}
-
-
-
-
 
 
 
@@ -84,113 +77,27 @@ function init() {
                 var popupWrapper = document.getElementById('popupWrapper');
 
                 myName = userNameInput.value;
-                isSignedIn = true;
                 document.getElementById('chat').style.removeProperty('display');
                 popupWrapper.style.display = "none"; //reset alertbox
+                isSignedIn = true;
+
+                messagesBuildComponent();
             }
         }
     }
-
-
-    if (isSignedIn) {
-        // send the message
-        $('#message_form').submit(function(e){
-            // prevent form from submitting
-            e.preventDefault();
-            // get message
-            var messageInput = document.getElementById("message");
-            var message = messageInput.value;
-        
-            console.log(message);
-        
-            //save in database
-            firebase.database().ref('messages').push().set({
-                "sender": myName,
-                "message": message
-            });
-        
-            // delete message after sending
-            messageInput.value = '';
-        });
-     
-        // DISPLAY IMAGES
-        //listen for incoming messages
-        firebase.database().ref("messages").on("child_added", function (snapshot) {
-            var htmlCustom = "";
-            var closeDiv = '</div>';
-        
-            // SENDER (Me)
-            if (snapshot.val().sender === myName) {
-                htmlCustom += buildChatBubbles.senderContainer;
-                    htmlCustom += "<button data-id='" + snapshot.key + "' class='delete_icon' onclick='deleteMessage(this)'></button>",
-                    htmlCustom += buildChatBubbles.senderBubble + snapshot.key + '">';
-                    htmlCustom += snapshot.val().sender + ": " + snapshot.val().message;
-                        htmlCustom += buildChatBubbles.senderTimeSent;
-                    htmlCustom += buildChatBubbles.senderPicContainer;
-                        htmlCustom += buildChatBubbles.senderPic;
-                    
-                    htmlCustom += closeDiv;
-                    htmlCustom += closeDiv;
-                    htmlCustom += closeDiv;
-                    htmlCustom += closeDiv;
-                    htmlCustom += closeDiv;
-                    
-            } else { // OTHER PEOPLE
-                htmlCustom += buildChatBubbles.receiverContainer;
-                    htmlCustom += buildChatBubbles.receiverPicContainer;
-                        htmlCustom += buildChatBubbles.receiverPic + closeDiv;
-                    htmlCustom += buildChatBubbles.receiverBubble + snapshot.key + '">';
-                    htmlCustom += snapshot.val().sender + ": " + snapshot.val().message;
-                        htmlCustom += buildChatBubbles.receiverTimeSent;
-                
-                htmlCustom += closeDiv;
-                htmlCustom += closeDiv;
-                htmlCustom += closeDiv;
-                htmlCustom += closeDiv;
-                htmlCustom += closeDiv;
-            }
-        
-        
-            //give each message a unique ID
-            //  htmlCustom += "<li id='message-" + snapshot.key + "'>";
-            //       //show delete vutton if message is sent by me
-            //       if (snapshot.val().sender === myName) {
-            //            htmlCustom += "<button data-id='" + snapshot.key + "' onclick='deleteMessage(this)'>";
-            //                 htmlCustom += "Delete"
-            //            htmlCustom += "</button>";
-            //       }
-            //  //display message
-            //  htmlCustom += snapshot.val().sender + ": " + snapshot.val().message;
-            //  htmlCustom += "</li>";
-        
-            document.getElementById("chat_wrapper").innerHTML += htmlCustom;
-            autoScrollDown();
-        });
-
-        //attach listener for delete message
-        firebase.database().ref("messages").on("child_removed", function (snapshot) {
-            // remove message node
-            document.getElementById("message-" + snapshot.key).innerHTML = "This message has been removed";
-        })
-    }
-
-
-
-
 
 function cycleBackground() {
     var imageDiv = document.getElementById('popupWrapper');
     var i = Math.floor(backgroundImages.length * Math.random());
 
-    if (isSignedIn === true) { //if Im signed in stop this function
-        return;
+    if (isSignedIn === false) { //if Im signed in stop this function
+        imageDiv.style.backgroundImage = "url(" + backgroundImages[i] + ")" + ", url(images/Logo_stamped.jpg)";
+        i = i + 1;
+        if (i == backgroundImages.length) {
+            i =  0;
+        }
     }
 
-    imageDiv.style.backgroundImage = "url(" + backgroundImages[i] + ")" + ", url(images/Logo_stamped.jpg)";
-    i = i + 1;
-    if (i == backgroundImages.length) {
-        i =  0;
-    }
 }
 
 function deleteMessage(self) {
